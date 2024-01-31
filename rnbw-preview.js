00a2edac86fc3e229d0090502a4d46c62f0b7877
atulbhatt-system32 <atulbhatt98@gmail.com>
2024-01-31 10:45:17 +0530

    reverse animation added

--- rnbw-preview.js
+++ rnbw-preview.js
@@ -263,114 +263,179 @@ class RnbwPreview extends HTMLElement {
 customElements.define("rnbw-preview", RnbwPreview);
 
 document.addEventListener("DOMContentLoaded", function () {
-  const rnbwPreviewElement = document.querySelector("rnbw-preview");
+    const rnbwPreviewElement = document.querySelector("rnbw-preview");
 
-  const options = {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.1,
-  };
+    const options = {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.1,
+    };
 
-  const observer = new IntersectionObserver(animateOnIntersect, options);
+    const observer = new IntersectionObserver(animateOnIntersect, options);
 
-  let text = "";
-  function animateOnIntersect(entries, observer) {
-    if (entries[0].isIntersecting) {
-      observer.unobserve(rnbwPreviewElement);
-      const h3 = document.querySelector("h3");
-      const spans = h3.querySelectorAll("span");
-      let index = 0;
-      let charIndex = 0;
+    let text = "";
+    let isAnimating = false;
 
-      function type() {
-        if (index < spans.length) {
-          spans[index].classList.remove("hidden");
-          const originalText = spans[index].getAttribute("data-text");
-          if (index === 0) {
-            let heading3DropdownIcon = document.getElementById(
-              "heading3-dropdown-icon"
-            );
-            heading3DropdownIcon.classList.remove("hidden");
-          }
-          if (charIndex < originalText.length) {
-            spans[index].textContent = originalText.slice(0, charIndex + 1);
-            charIndex++;
-            setTimeout(type, 20);
-          } else {
-            charIndex = 0;
-            index++;
-            setTimeout(type, 800);
-          }
-        } else {
-          setTimeout(() => {
-            reset();
-          }, 4000);
-        }
-      }
-      const code = document.querySelector("code");
-      if (!text) {
-        text = code.textContent;
-      }
-      code.textContent = "";
+    function animateOnIntersect(entries, observer) {
+        if (entries[0].isIntersecting && !isAnimating) {
+            isAnimating = true;
+            observer.unobserve(rnbwPreviewElement);
+            const h3 = document.querySelector("h3");
+            const spans = h3.querySelectorAll("span");
+            let index = 0;
+            let charIndex = 0;
 
-      let i = 0;
-      let spanCount = 1;
-      let totalSpansAnimated = 0;
-      let timer = 100;
-      function typeCode() {
-        if (i < text.length) {
-          code.textContent += text.charAt(i);
-          i++;
-          if (text.charAt(i) === ".") {
-            if (spanCount <= 7) {
-              let element = document.getElementById("span" + spanCount);
-              setTimeout(() => {
-                element.style.opacity = 1;
-                totalSpansAnimated++;
-              }, timer);
-              timer += 70;
-              spanCount++;
+            function type() {
+                if (index < spans.length) {
+                    spans[index].classList.remove("hidden");
+                    const originalText = spans[index].getAttribute("data-text");
+                    if (index === 0) {
+                        let heading3DropdownIcon = document.getElementById(
+                            "heading3-dropdown-icon"
+                        );
+                        heading3DropdownIcon.classList.remove("hidden");
+                    }
+                    if (charIndex < originalText.length) {
+                        spans[index].textContent = originalText.slice(0, charIndex + 1);
+                        charIndex++;
+                        setTimeout(type, 40);
+                    } else {
+                        charIndex = 0;
+                        index++;
+                        setTimeout(type, 800);
+                    }
+                } else {
+                    setTimeout(() => {
+                        reverseAnimation();
+                    }, 2500); // Wait for initial animation to complete
+                }
             }
-            setTimeout(typeCode, 70);
-          } else {
-            setTimeout(typeCode, 15); // adjust the delay time as needed
-          }
+
+            const code = document.querySelector("code");
+            if (!text) {
+                text = code.textContent;
+            }
+            code.textContent = "";
+
+            let i = 0;
+            let spanCount = 1;
+            let totalSpansAnimated = 0;
+            let timer = 100;
+            function typeCode() {
+                if (i < text.length) {
+                    code.textContent += text.charAt(i);
+                    i++;
+                    if (text.charAt(i) === ".") {
+                        if (spanCount <= 7) {
+                            let element = document.getElementById("span" + spanCount);
+                            setTimeout(() => {
+                                element.style.opacity = 1;
+                                totalSpansAnimated++;
+                            }, timer);
+                            timer += 70;
+                            spanCount++;
+                        }
+                        setTimeout(typeCode, 70);
+                    } else {
+                        setTimeout(typeCode, 15); // adjust the delay time as needed
+                    }
+                }
+            }
+
+            typeCode();
+            spans.forEach((span) => span.setAttribute("data-text", span.textContent));
+            spans.forEach((span) => (span.textContent = ""));
+            type();
         }
+    }
+
+    function reset() {
+        console.log("reset function is called")
+        const spans = document.querySelectorAll("h3 span");
+        spans.forEach((span) => span.classList.add("hidden"));
+    
+        let heading3DropdownIcon = document.getElementById(
+          "heading3-dropdown-icon"
+        );
+        heading3DropdownIcon.classList.add("hidden");
+    
+        for (let i = 1; i <= 7; i++) {
+          let element = document.getElementById("span" + i);
+          element.style.opacity = 0;
+        }
+        const code = document.querySelector("code");
+        code.textContent = "";
+        observer.unobserve(rnbwPreviewElement);
+        index = 0;
+        charIndex = 0;
+        i = 0;
+        spanCount = 1;
+        totalSpansAnimated = 0;
+        timer = 1500;
+        observer.observe(rnbwPreviewElement);
       }
 
-      typeCode();
-      spans.forEach((span) => span.setAttribute("data-text", span.textContent));
-      spans.forEach((span) => (span.textContent = ""));
-      type();
+    function reverseAnimation() {
+        
+        const spans = document.querySelectorAll("h3 span");
+        const code = document.querySelector("code");
+        if (!text) {
+            text = code.textContent;
+        }
+
+        let index = spans.length - 1;
+        let charIndex = spans[index].textContent.length - 1;
+        let i = code.textContent.length - 1;
+
+        function reverseText() {
+            if (index >= 0) {
+                if (charIndex >= 0) {
+                    spans[index].textContent = spans[index].textContent.slice(0, charIndex);
+                    charIndex--;
+                    setTimeout(reverseText, 40);
+                } else {
+                    index--;
+                    charIndex = spans[index].textContent.length - 1;
+                    setTimeout(reverseText, 800);
+                }
+            }
+        }
+
+        
+            let spanCount = 8;
+            let totalSpansAnimated = 8;
+            let timer = 100;
+        function reverseCode() {
+            if (i >= 0) {
+                code.textContent = code.textContent.slice(0, i);
+                i--;
+                if(text.charAt(i) === ".") {
+                    if (spanCount >=0 ) {
+                        let element = document.getElementById("span" + spanCount);
+                        setTimeout(() => {
+                            element.style.opacity = 0;
+                            totalSpansAnimated--;
+                        }, timer);
+                        timer += 70;
+                        spanCount--;
+                    }
+                    setTimeout(reverseCode, 100);
+                } else {
+                    setTimeout(reverseCode, 10); // adjust the delay time as needed
+                }
+                
+            }
+            else {
+                setTimeout(() => {
+                  reset();
+                }, 500);
+              }
+        }
+
+        reverseText();
+        reverseCode();
+
     }
-  }
-  function reset() {
-    const spans = document.querySelectorAll("h3 span");
-    spans.forEach((span) => span.classList.add("hidden"));
 
-    let heading3DropdownIcon = document.getElementById(
-      "heading3-dropdown-icon"
-    );
-    heading3DropdownIcon.classList.add("hidden");
-
-    for (let i = 1; i <= 7; i++) {
-      let element = document.getElementById("span" + i);
-      element.style.opacity = 0;
-    }
-    const code = document.querySelector("code");
-    code.textContent = "";
-    observer.unobserve(rnbwPreviewElement);
-    index = 0;
-    charIndex = 0;
-    i = 0;
-    spanCount = 1;
-    totalSpansAnimated = 0;
-    timer = 1500;
-
-    setTimeout(() => {
-      observer.observe(rnbwPreviewElement);
-    }, 1500);
-  }
-
-  observer.observe(rnbwPreviewElement);
+    observer.observe(rnbwPreviewElement);
 });
